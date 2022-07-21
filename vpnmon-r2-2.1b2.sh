5cbad065b17a5863d55a2b31d4979bba0a7132f6
ViktorJp <97465574+ViktorJp@users.noreply.github.com>
2022-07-20 22:41:35 -0400

    Update vpnmon-r2-2.1b2.sh

--- vpnmon-r2-2.1b2.sh
+++ vpnmon-r2-2.1b2.sh
@@ -515,7 +515,6 @@ checkwan () {
     # Check the actual WAN State from NVRAM before running connectivity test, or insert itself into loop after failing an SSL handshake test
     if [ "$($timeoutcmd$timeoutsec nvram get wan0_state_t)" -eq 2 ] || [ "$($timeoutcmd$timeoutsec nvram get wan1_state_t)" -eq 2 ]
       then
-        echo -e "WAN STATE: $($timeoutcmd$timeoutsec nvram get wan0_state_t) $($timeoutcmd$timeoutsec nvram get wan1_state_t)"
 
         # Test the active WAN connection using 443 and verifying a handshake... if this fails, then the WAN connection is most likely down... or Google is down ;)
         if ($timeoutcmd$timeoutlng nc -w1 $testssl 443 >/dev/null 2>&1 && echo | $timeoutcmd$timeoutlng openssl s_client -connect $testssl:443 >/dev/null 2>&1 |awk 'handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 } $1 $2 == "SSLhandshake" { handshake = 1 }') >/dev/null 2>&1
